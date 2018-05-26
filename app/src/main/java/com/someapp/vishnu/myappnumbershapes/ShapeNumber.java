1a89425e0974b77b88fd64bb9f2c5ec259e999cd
Vishnu Adda <addavishnu@gmail.com>
2018-05-25 22:12:59 -0700

    Initial commit

---
+++ app/src/main/java/com/someapp/vishnu/myappnumbershapes/ShapeNumber.java
@@ -0,0 +1,16 @@
+package com.someapp.vishnu.myappnumbershapes;
+
+public class ShapeNumber {
+
+    int number;
+
+    public void setNumber(int num) { number = num; } // Setter method
+    public int getNumber() { return number; } // Getter method
+
+    public boolean isTriangular() {
+
+        return false;
+
+    }
+
+}
